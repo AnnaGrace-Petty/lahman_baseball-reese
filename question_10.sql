860bed19881827adc1748856a03888933db009d5
AnnaGrace-Petty <annagracepetty@comcast.net>
2022-10-06 15:27:50 -0500

    Finished

--- question_10.sql
+++ question_10.sql
@@ -2,23 +2,23 @@
 WITH year_count AS (SELECT playerid, COUNT(DISTINCT(yearid)) AS total_years
 					FROM batting
 					GROUP BY playerid),
-  career_high_hr AS (SELECT playerid, yearid, max(hr) AS hr_count
-					 FROM batting
-					 GROUP BY playerid, yearid),
-  year_count_10 AS (SELECT b.playerid, ch.hr_count
+  career_high_hr AS (SELECT playerid, max(hr) AS hr_count
+					 FROM batting 
+					 WHERE hr > 0
+					 GROUP BY playerid),
+  year_count_10 AS (SELECT b.playerid, b.hr
 				    FROM batting AS b
 				    INNER JOIN year_count AS yc
 				    USING(playerid)
-					INNER JOIN career_high_hr AS ch
-					on b.playerid = ch.playerid
-						 AND b.yearid = ch.yearid
 				    WHERE yc.total_years >= 10
-				    	AND ch.yearid = 2016
-					GROUP BY b.playerid, ch.hr_count)
-SELECT p.namefirst, p.namelast, y.hr_count
+				    	AND b.yearid = 2016)
+SELECT p.namefirst, p.namelast, y.hr
 FROM year_count_10 AS y
 INNER JOIN people AS p
 USING(playerid)
+INNER JOIN career_high_hr
+USING(playerid)
+WHERE career_high_hr.hr_count = y.hr
 
 
 
